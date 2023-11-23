2685942d120c30bda9920c6574605c753af91696
daviu <daviu@DESKTOP-0BEA9Q2>
2023-11-23 19:30:41 -0300

    Adicionar arquivos de projeto.

---
+++ Triangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseObjetoEAtributoTriangulo
+{
+    internal class Triangulo
+    {
+        //classe e a definição de um tipo 
+
+
+        //nome de atributo sempre com primeira letra maiuscula.
+
+
+        //public siginifica que esse atributo pode ser acionado em outro arquivo
+
+        //objetos são instâncias da classe.
+        
+        public double A;
+        public double B; 
+        public double C;
+
+
+
+    }
+}
